9edf423022bf46c155e92fe552f61bd54274cfff
mrzhang638993 <1446719386@qq.com>
2020-08-12 16:09:11 +0800

    增加hbase的scan查询，scan的filter查询，familyFilter查询操作

--- src/main/java/cn/itcast/zookeeper_api/hbase/HbaseOperate.java
+++ src/main/java/cn/itcast/zookeeper_api/hbase/HbaseOperate.java
@@ -1,11 +1,10 @@
 package cn.itcast.zookeeper_api.hbase;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.HColumnDescriptor;
-import org.apache.hadoop.hbase.HTableDescriptor;
-import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.*;
 import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.filter.BinaryComparator;
+import org.apache.hadoop.hbase.filter.RowFilter;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.junit.After;
 import org.junit.Before;
@@ -19,6 +18,7 @@ public class HbaseOperate {
 
     private  Connection connection;
     private Admin admin;
+    private  Table table;
 
     @Before
     public   void init() throws IOException {
@@ -50,14 +50,14 @@ public class HbaseOperate {
      * */
     @Test
     public   void addData() throws IOException {
-        Table myuser = connection.getTable(TableName.valueOf("myuser"));
+        table = connection.getTable(TableName.valueOf("myuser"));
         Put put = new Put("0001".getBytes());
         put.addColumn("f1".getBytes(),"id".getBytes(), Bytes.toBytes(1));
         put.addColumn("f1".getBytes(),"name".getBytes(),Bytes.toBytes("zhangsan"));
         put.addColumn("f1".getBytes(),"age".getBytes(),Bytes.toBytes(18));
         put.addColumn("f2".getBytes(),"address".getBytes(),Bytes.toBytes("地球人"));
         put.addColumn("f2".getBytes(),"phone".getBytes(),Bytes.toBytes("13392112455"));
-        myuser.put(put);
+        table.put(put);
     }
 
     /**
@@ -66,7 +66,7 @@ public class HbaseOperate {
     @Test
     public void insertBatchData() throws IOException {
         //获取表
-        Table myuser = connection.getTable(TableName.valueOf("myuser"));
+        table = connection.getTable(TableName.valueOf("myuser"));
         //创建put对象，并指定rowkey
         Put put = new Put("0002".getBytes());
         put.addColumn("f1".getBytes(),"id".getBytes(),Bytes.toBytes(1));
@@ -130,16 +130,93 @@ public class HbaseOperate {
         listPut.add(put4);
         listPut.add(put5);
         listPut.add(put6);
-        myuser.put(listPut);
-        myuser.close();
+        table.put(listPut);
+        table.close();
     }
 
     /**
      * hbase查询数据操作
+     * 根据rowkey进行查询操作
      * */
     @Test
-    public void  queryData(){
-         // 查询数据
+    public void  queryData() throws IOException {
+        table = connection.getTable(TableName.valueOf("myuser"));
+         //  对应的是不bytes的代码的,0003对应的是rowkey的
+        Get get=new Get("0003".getBytes());
+        //  查询操作的时候增加列族查询操作实现
+        get.addFamily("f1".getBytes());
+        //  设置列族f1，以及列的名称id的数值
+        get.addColumn("f1".getBytes(),"id".getBytes());
+        //  封装了所有的结果数据的。
+        Result result = table.get(get);
+        //  获取结果中所有的cells数值
+        printResult(result);
+    }
+
+    /**
+     * 通过scan进行查询操作
+     * */
+    @Test
+    public void scanQuery() throws IOException {
+        table = connection.getTable(TableName.valueOf("myuser"));
+        Scan scan=new Scan();
+        //  scan扫描的话，包括前面的0004 不包括后面的0006的。全表扫描的话就是没有任何的条件的过滤的
+        scan.setStartRow("0004".getBytes());
+        scan.setStopRow("0006".getBytes());
+        ResultScanner scanner = table.getScanner(scan);
+        for (Result result :scanner) {
+            printResult(result);
+        }
+    }
+
+    /**
+     * scan的过滤器查询.
+     * 通过过滤器，可以实现更加强大的hbase的过滤功能和实现逻辑
+     * 小于0003的
+     * */
+    @Test
+    public  void  filterQuery() throws IOException {
+        table = connection.getTable(TableName.valueOf("myuser"));
+        Scan scan=new Scan();
+        RowFilter rowFilter = new RowFilter(CompareOperator.LESS, new BinaryComparator(Bytes.toBytes("0003")));
+        scan.setFilter(rowFilter);
+        ResultScanner results = table.getScanner(scan);
+        for(Result result:results){
+            printResult(result);
+        }
+    }
+
+    /**
+     * scan的进一步的过滤操作实现
+     * */
+    @Test
+    public void familyFilter(){
+       // 对应的实现相关的代码查询操作
+    }
+
+    private  void  printResult(Result result){
+        List<Cell> cells = result.listCells();
+        for (Cell cell :cells) {
+            // 获取cell对应的rowkey
+            String rowkey= Bytes.toString(cell.getRowArray(), cell.getRowOffset(), cell.getRowLength());
+            System.out.println(rowkey);
+            //  获取cell对应的列族的名称
+            String columnArray = Bytes.toString(cell.getFamilyArray(), cell.getFamilyOffset(), cell.getFamilyLength());
+            System.out.println(columnArray);
+            //  获取列的名称
+            String columnName = Bytes.toString(cell.getQualifierArray(), cell.getQualifierOffset(), cell.getQualifierLength());
+            if (columnArray.equals("f1") && (columnName.equals("id") | columnName.equals("age"))) {
+                int value = Bytes.toInt(cell.getValueArray(), cell.getValueOffset(), cell.getValueLength());
+                //获取到列对应的数值
+                System.out.println(value);
+            } else {
+                String value = Bytes.toString(cell.getValueArray(), cell.getValueOffset(), cell.getValueLength());
+                //获取到列对应的数值
+                System.out.println(value);
+            }
+            // 获取列的名称
+            System.out.println(columnName);
+        }
     }
 
     @After
@@ -149,6 +226,9 @@ public class HbaseOperate {
         }
         if (connection!=null){
             connection.close();
+        }
+        if (table!=null){
+            table.close();
         }
     }
 }

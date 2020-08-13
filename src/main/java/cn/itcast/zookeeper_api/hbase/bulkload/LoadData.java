2f603e99780e6d7223943f05a2bec62e0a70d03a
mrzhang638993 <1446719386@qq.com>
2020-08-13 19:22:34 +0800

    增加hbase  bulkloaddata

---
+++ src/main/java/cn/itcast/zookeeper_api/hbase/bulkload/LoadData.java
@@ -0,0 +1,31 @@
+package cn.itcast.zookeeper_api.hbase.bulkload;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.tool.LoadIncrementalHFiles;
+
+
+/**
+ * 加载数据到hbase中
+ * */
+public class LoadData {
+
+    public static void main(String[] args) throws Exception {
+        System.setProperty("HADOOP_USER_NAME", "root");
+        Configuration configuration = HBaseConfiguration.create();
+        configuration.set("hbase.zookeeper.property.clientPort", "2181");
+        configuration.set("hbase.zookeeper.quorum", "node01,node02,node03");
+
+        Connection connection =  ConnectionFactory.createConnection(configuration);
+        Admin admin = connection.getAdmin();
+        Table table = connection.getTable(TableName.valueOf("myuser2"));
+        LoadIncrementalHFiles load = new LoadIncrementalHFiles(configuration);
+        load.doBulkLoad(new Path("hdfs://node01:8020/hbase/hfile_out"), admin,table,connection.getRegionLocator(TableName.valueOf("myuser2")));
+    }
+}

93a8aefd0ff0213b1ad94fe4f77e41ac05218007
mrzhang638993 <1446719386@qq.com>
2020-08-15 22:01:03 +0800

    完成kafka的学习

--- src/main/java/cn/itcast/zookeeper_api/kafka/MyProducer.java
+++ src/main/java/cn/itcast/zookeeper_api/kafka/MyProducer.java
@@ -13,10 +13,13 @@ public class MyProducer {
     public static void main(String[] args) {
         Properties props=new Properties();
         props.put("bootstrap.servers", "node01:9092,node02:9092,node03:9092");
+        //  消息的确认机制，可以保证消息的不丢失的，消息丢失的话，会报错的。这个参数很关键的。
         props.put("acks", "all");
+        //  确定消息发送失败之后的尝试尝试，可以设置为3的。
         props.put("retries", 0);
         props.put("batch.size", 16384);
         props.put("linger.ms", 1);
+        // 异步确认的时候，可以设置buffer的大小的。
         props.put("buffer.memory", 33554432);
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");

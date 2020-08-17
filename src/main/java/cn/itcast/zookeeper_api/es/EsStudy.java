8a2b004426d52de76885735742c25b61146fabeb
mrzhang638993 <1446719386@qq.com>
2020-08-17 14:13:35 +0800

    增加es查询的准备的数据

--- src/main/java/cn/itcast/zookeeper_api/es/EsStudy.java
+++ src/main/java/cn/itcast/zookeeper_api/es/EsStudy.java
@@ -2,21 +2,22 @@ package cn.itcast.zookeeper_api.es;
 
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.google.inject.internal.cglib.core.$ClassEmitter;
 import org.elasticsearch.action.ActionFuture;
+import org.elasticsearch.action.admin.indices.mapping.put.PutMappingRequest;
 import org.elasticsearch.action.bulk.BulkRequestBuilder;
 import org.elasticsearch.action.index.IndexRequestBuilder;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
+import org.elasticsearch.client.Requests;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
-import org.elasticsearch.common.xcontent.XContent;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
-import org.jcodings.util.Hash;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -37,7 +38,7 @@ public class EsStudy {
     /**
      * 获取客户端对象
      * */
-    @Before
+    //@Before
     public void  getConnect() throws UnknownHostException {
         Settings settings = Settings.builder().put("cluster.name","myes").build();
         TransportAddress node01 = new TransportAddress(InetAddress.getByName("node01"), 9300);
@@ -192,4 +193,93 @@ public class EsStudy {
     public void  deleteAllIndex(){
         client.admin().indices().prepareDelete("myindex1").execute().actionGet();
     }
+
+
+    /**
+     *  给索引查询增加数据源
+     * */
+    @Test
+    public void   queryIndex() throws IOException {
+        Settings settings = Settings.builder().
+                put("cluster.name","myes")
+                //  开启es的嗅探机制
+                .put("client.transport.sniff",true)
+                .build();
+        TransportAddress node01 = new TransportAddress(InetAddress.getByName("node01"), 9300);
+        TransportAddress node02 = new TransportAddress(InetAddress.getByName("node02"), 9300);
+        TransportAddress node03 = new TransportAddress(InetAddress.getByName("node03"), 9300);
+        client=new PreBuiltTransportClient(settings).
+                addTransportAddress(node01)
+                .addTransportAddress(node02)
+                .addTransportAddress(node03);
+        XContentBuilder mapping=XContentFactory.jsonBuilder()
+                .startObject()
+                .startObject("properties")
+                //  这个id是数据id的内容，不是系统的id属性的
+                .startObject("id").field("type","integer").endObject()
+                .startObject("name").field("type", "text").field("analyzer", "ik_max_word").endObject()
+                .startObject("age").field("type", "integer").endObject()
+                .startObject("sex").field("type", "text").field("analyzer", "ik_max_word").endObject()
+                .startObject("address").field("type", "text").field("analyzer", "ik_max_word").endObject()
+                .startObject("phone").field("type", "text").endObject()
+                .startObject("email").field("type", "text").endObject()
+                .startObject("say").field("type", "text").field("analyzer", "ik_max_word").endObject()
+                .endObject()
+                .endObject();
+        PutMappingRequest putmap = Requests.putMappingRequest("indexsearch").type("mysearch").source(mapping);
+        //创建索引
+        client.admin().indices().prepareCreate("indexsearch").execute().actionGet();
+        //为索引添加映射
+        client.admin().indices().putMapping(putmap).actionGet();
+
+
+        BulkRequestBuilder bulkRequestBuilder = client.prepareBulk();
+        Person lujunyi = new Person(2, "玉麒麟卢俊义", 28, 1, "水泊梁山", "17666666666", "lujunyi@itcast.com","hello world今天天气还不错");
+        Person wuyong = new Person(3, "智多星吴用", 45, 1, "水泊梁山", "17666666666", "wuyong@itcast.com","行走四方，抱打不平");
+        Person gongsunsheng = new Person(4, "入云龙公孙胜", 30, 1, "水泊梁山", "17666666666", "gongsunsheng@itcast.com","走一个");
+        Person guansheng = new Person(5, "大刀关胜", 42, 1, "水泊梁山", "17666666666", "wusong@itcast.com","我的大刀已经饥渴难耐");
+        Person linchong = new Person(6, "豹子头林冲", 18, 1, "水泊梁山", "17666666666", "linchong@itcast.com","梁山好汉");
+        Person qinming = new Person(7, "霹雳火秦明", 28, 1, "水泊梁山", "17666666666", "qinming@itcast.com","不太了解");
+        Person huyanzhuo = new Person(8, "双鞭呼延灼", 25, 1, "水泊梁山", "17666666666", "huyanzhuo@itcast.com","不是很熟悉");
+        Person huarong = new Person(9, "小李广花荣", 50, 1, "水泊梁山", "17666666666", "huarong@itcast.com","打酱油的");
+        Person chaijin = new Person(10, "小旋风柴进", 32, 1, "水泊梁山", "17666666666", "chaijin@itcast.com","吓唬人的");
+        Person zhisheng = new Person(13, "花和尚鲁智深", 15, 1, "水泊梁山", "17666666666", "luzhisheng@itcast.com","倒拔杨垂柳");
+        Person wusong = new Person(14, "行者武松", 28, 1, "水泊梁山", "17666666666", "wusong@itcast.com","二营长。。。。。。");
+
+        bulkRequestBuilder.add(client.prepareIndex("indexsearch", "mysearch", "1")
+                .setSource(JSONObject.toJSONString(lujunyi), XContentType.JSON)
+        );
+        bulkRequestBuilder.add(client.prepareIndex("indexsearch", "mysearch", "2")
+                .setSource(JSONObject.toJSONString(wuyong), XContentType.JSON)
+        );
+        bulkRequestBuilder.add(client.prepareIndex("indexsearch", "mysearch", "3")
+                .setSource(JSONObject.toJSONString(gongsunsheng), XContentType.JSON)
+        );
+        bulkRequestBuilder.add(client.prepareIndex("indexsearch", "mysearch", "4")
+                .setSource(JSONObject.toJSONString(guansheng), XContentType.JSON)
+        );
+        bulkRequestBuilder.add(client.prepareIndex("indexsearch", "mysearch", "5")
+                .setSource(JSONObject.toJSONString(linchong), XContentType.JSON)
+        );
+        bulkRequestBuilder.add(client.prepareIndex("indexsearch", "mysearch", "6")
+                .setSource(JSONObject.toJSONString(qinming), XContentType.JSON)
+        );
+        bulkRequestBuilder.add(client.prepareIndex("indexsearch", "mysearch", "7")
+                .setSource(JSONObject.toJSONString(huyanzhuo), XContentType.JSON)
+        );
+        bulkRequestBuilder.add(client.prepareIndex("indexsearch", "mysearch", "8")
+                .setSource(JSONObject.toJSONString(huarong), XContentType.JSON)
+        );
+        bulkRequestBuilder.add(client.prepareIndex("indexsearch", "mysearch", "9")
+                .setSource(JSONObject.toJSONString(chaijin), XContentType.JSON)
+        );
+        bulkRequestBuilder.add(client.prepareIndex("indexsearch", "mysearch", "10")
+                .setSource(JSONObject.toJSONString(zhisheng), XContentType.JSON)
+        );
+        bulkRequestBuilder.add(client.prepareIndex("indexsearch", "mysearch", "11")
+                .setSource(JSONObject.toJSONString(wusong), XContentType.JSON)
+        );
+        bulkRequestBuilder.get();
+    }
+
 }

8667b946fd26e083d233ab575ef5f5843577c4d2
mrzhang638993 <1446719386@qq.com>
2020-08-23 22:03:13 +0800

    完成泛型下界的操作实现

---
+++ scala/src/com/itheima/scala/extractor/SuperMan.scala
@@ -0,0 +1,3 @@
+package com.itheima.scala.extractor
+
+class SuperMan  extends  PoliceMan()
--- scala/src/com/itheima/scala/extractor/_17GernericType.scala
+++ scala/src/com/itheima/scala/extractor/_17GernericType.scala
@@ -3,6 +3,7 @@ package com.itheima.scala.extractor
 object _17GernericType {
 
   /**
+   * 定义上界操作
    * 限定泛型类型是person本身或者是person的子类的
    * */
   def  demo[T <:Person](array: Array[T])=println(array)

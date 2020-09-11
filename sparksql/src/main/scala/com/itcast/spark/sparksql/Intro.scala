e02d93b54666ee1b033bc9686ccfcd2efd673596
mrzhang638993 <1446719386@qq.com>
2020-09-11 08:48:48 +0800

    增加dataset,dataframe的数据集操作

--- sparksql/src/main/scala/com/itcast/spark/sparksql/Intro.scala
+++ sparksql/src/main/scala/com/itcast/spark/sparksql/Intro.scala
@@ -1,6 +1,7 @@
 package com.itcast.spark.sparksql
 
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 import org.apache.spark.{SparkConf, SparkContext, sql}
 import org.junit.Test
@@ -58,6 +59,66 @@ class Intro {
     frame.createOrReplaceTempView("person")
     val nameFrame: DataFrame = sparkSql.sql("select name from  person where age>10 and age<20")
     nameFrame.show()
+  }
+  /**
+   * 测试数据集的数据操作
+   * dataset中存放的是结构化的数据的。可以使用类似sql的方式实现查询的
+   * dataset是存在类型的。dataset对应的还存在schema的。类型安全的容器。
+   * */
+  @Test
+  def  testDataSet(): Unit ={
+     //  创建全局的sparkSession
+     val spark = new SparkSession.Builder().master("local[6]").appName("dataset1").getOrCreate()
+    import spark.implicits._
+    // 演示dataset的操作结果
+    val rdd: RDD[Person] = spark.sparkContext.parallelize(Seq(Person("zhangsan", 15), Person("lisi", 20), Person("wangwu", 30)))
+    val dataSF: Dataset[Person] = rdd.toDS()
+    //  支持强类型的语言查询的
+    val filterDateSf: Dataset[Person] = dataSF.filter(item => item.age > 10)
+    //  支持弱类型的查询
+    //  可以直接编写sql的表达式进行查询操作
+    dataSF.filter("age>10").show()
+    dataSF.filter($"age">10).show()
+  }
+  /**
+   * 执行物理的RDD的优化操作
+   * */
+  @Test
+  def   testRdd(): Unit ={
+    val spark = new SparkSession.Builder().master("local[6]").appName("dataset1").getOrCreate()
+    import spark.implicits._
+    // 演示dataset的操作结果，创建的方式还是比较的古老的
+   /* val rdd: RDD[Person] = spark.sparkContext.parallelize(Seq(Person("zhangsan", 15), Person("lisi", 20), Person("wangwu", 30)))
+    val dataset: Dataset[Person] = rdd.toDS()*/
+    //
+    val  dataset=spark.createDataset(Seq(Person("zhangsan", 15), Person("lisi", 20), Person("wangwu", 30)))
+    /**
+     * 查看物理执行计划的本质操作
+     * 将生成的物理计划转化为可执行的RDD操作。
+     * 物理执行计划经过处理之后对应的数据都会转化为InteralRow的数据的
+     * 将dataset的类型保留下来的，形成一个同样的数据类型的。
+     * */
+      // 直接获取到已经分析过的物理执行计划，得到对应的分析过的rdd的数据的
+    val resultRdd: RDD[InternalRow] = dataset.queryExecution.toRdd
+    //  获取dataset中的rdd数据的。对应的是从dataset的InternalRow通过decode中获取数据转化成为RDD[Person]的。可以获取到底层的rdd，从而进行rdd的操作的
+    val rdd: RDD[Person] = dataset.rdd
+
+    println("物理执行计划的rdd:"+resultRdd.toDebugString)
+    println("dataset的rdd的数据:"+rdd.toDebugString)
+  }
+  /**
+   * 测试dataFrame的操作的
+   * dataframe借鉴的是pandas的datframe的数据结构的
+   * */
+  @Test
+  def  testDataFrame(): Unit ={
+    val spark: SparkSession = SparkSession.builder().master("local[6]").appName("testFramme").getOrCreate()
+     import spark.implicits._
+    val frame: DataFrame = Seq(Person("zhangsan", 20), Person("lisi", 25), Person("wangwu", 30)).toDF()
+    frame.where("age>20")
+      .select("name")
+      .show()
+
   }
 }
 
--- sparksql/src/main/scala/com/itcast/spark/sparksql/Readme.txt
+++ sparksql/src/main/scala/com/itcast/spark/sparksql/Readme.txt
@@ -6,3 +6,18 @@ spark-shell的代码操作:
 1.不要出现返回值的类型；
 2.对应的是上下文为spark的，是默认的。
 
+dataset是支持优化的。
+
+spark是一个比较成熟的工具的。可以在一段代码中使用了rdd,又可以使用dataset,还可以使用SQL语句执行操作的。
+spark对应的是一个比较弹性的工具的。
+dataset.rdd 可以将dataset转化为rdd操作的。
+
+Dataset 是一个强类型, 并且类型安全的数据容器, 并且提供了结构化查询 API 和类似 RDD 一样的命令式 API。类似于java中的list等的。
+DataFrame类似于关系型数据库的一张表，在dataFrame上的操作非常类似于sql语句。dataFrame其中包含了行和列的数据的，并且列具有schema的约束信息的。
+dataFrame有行和列，还有schema的结构信息。
+
+一般的处理数据的过程对应的都是ETL的过程的。ETL代表的是extract(抽取),T(处理和转换操作),L(装载和落地)。
+
+
+
+

1de672d2dfbfc818d01c63fc2dbf0797f96feb80
mrzhang638993 <1446719386@qq.com>
2020-09-19 17:27:02 +0800

    完成相关的开发任务操作实现的

--- sparksql/src/main/scala/com/itcast/spark/sparksql/TypeTransformation.scala
+++ sparksql/src/main/scala/com/itcast/spark/sparksql/TypeTransformation.scala
@@ -12,21 +12,23 @@ class TypeTransformation {
     // 集群模式下面是不需要设置master的，集群里面是有自己的master的。
     .master("local[6]")
     .getOrCreate()
+
   import spark.implicits._
+
   /**
    * 执行filter操作实现
-   * */
+   **/
   @Test
-  def  testFilterRdd(): Unit ={
+  def testFilterRdd(): Unit = {
     val ds: Dataset[Person] = Seq(Person("zhangsan", 20), Person("lisi", 15), Person("wangwu", 25)).toDS()
-    ds.filter(person=>person.age>15).show()
+    ds.filter(person => person.age > 15).show()
   }
 
   /**
    * 执行聚合操作.groupBy
-   * */
+   **/
   @Test
-  def testAggr(): Unit ={
+  def testAggr(): Unit = {
     val ds: Dataset[Person] = Seq(Person("zhangsan", 20), Person("zhangsan", 15), Person("wangwu", 25)).toDS()
     //  根据person.name作为key执行group计算的。
     // String 对应的是key的类型的，Person对应的是value的类型的
@@ -39,9 +41,9 @@ class TypeTransformation {
 
   /**
    * 测试split算子执行操作
-   * */
+   **/
   @Test
-  def  execSplit(): Unit ={
+  def execSplit(): Unit = {
     // 执行的是split的操作实现的。
     val range: Dataset[lang.Long] = spark.range(15)
     // randowSplit的操作的.根据权重切割为多少份的数据的。
@@ -51,28 +53,28 @@ class TypeTransformation {
     array.foreach(_.show())
 
     // 抽样进行操作实现和管理操作的。false对应的代表的是无放回的操作的。fraction对应的是采样比的操作的。
-    range.sample(false,0.6).show()
+    range.sample(false, 0.6).show()
   }
 
   /**
    * orderBy  sort排序操作
-   * */
+   **/
   @Test
-  def  testSort(): Unit ={
+  def testSort(): Unit = {
     val ds: Dataset[Person] = Seq(Person("zhangsan", 12), Person("zhangsan", 8), Person("wangwu", 15)).toDS()
     //  指定升序和降序操作的
-    ds.orderBy(new Column("name").desc,new Column("age").asc).show()
+    ds.orderBy(new Column("name").desc, new Column("age").asc).show()
     //  还可以使用sort方法剩下相关的操作的
-    ds.sort(new Column("name").desc,new Column("age").asc).show()
+    ds.sort(new Column("name").desc, new Column("age").asc).show()
   }
 
   /**
    * 去重操作的算子
    * dictinct
    * dropDuplicates
-   * */
+   **/
   @Test
-  def quitDouble(): Unit ={
+  def quitDouble(): Unit = {
     // 执行去重操作实现
     val ds: Dataset[Person] = Seq(Person("zhangsan", 15), Person("zhangsan", 15), Person("lisi", 15)).toDS()
     // 去重重复row的数据的。每一行对应的是一个Person对象的。
@@ -85,9 +87,9 @@ class TypeTransformation {
 
   /**
    * 集合操作，交集，差集，并集以及limit执行操作
-   * */
+   **/
   @Test
-  def collection(): Unit ={
+  def collection(): Unit = {
     //  创建集合操作1
     val set1: Dataset[lang.Long] = spark.range(15)
     // 创建集合操作2
@@ -109,9 +111,9 @@ class TypeTransformation {
 
   /**
    * 无类型的算子，对应的是使用相关的schema的信息执行操作的
-   * */
+   **/
   @Test
-  def  testSchema(): Unit ={
+  def testSchema(): Unit = {
     val persons = Seq(Person("zhangsan", 12), Person("lisi", 18), Person("zhansgan", 8)).toDS()
     //  sql语句的操作不是应该从select上面的。真正的操作是from进行操作的。
     persons.
@@ -127,36 +129,36 @@ class TypeTransformation {
 
   /**
    * 无类型的计算算子
-   * */
+   **/
   @Test
-  def testNonSchema(): Unit ={
+  def testNonSchema(): Unit = {
     // 测试新建列，重命名列的数据
     val persons = Seq(Person("zhangsan", 12), Person("lisi", 18), Person("zhansgan", 8)).toDS()
     // 对应的select语句中选择执行sql语句进行操作
     import org.apache.spark.sql.functions._
     // 新建列名称
-    persons.withColumn("random",expr("rand()")).show()
+    persons.withColumn("random", expr("rand()")).show()
     // 增加列的操作.name_column对应的数据是列的数据的
-    persons.withColumn("name_column",expr("name")).show()
+    persons.withColumn("name_column", expr("name")).show()
     // 增加列的操作的，对应的判断每一列的数据是否是等于“”的数据的。增加新的列进行操作的。
-    persons.withColumn("name_jok",new Column("name")==="").show()
+    persons.withColumn("name_jok", new Column("name") === "").show()
     //重命名操作实现和管理实现
-    persons.withColumnRenamed("name_new","name")
+    persons.withColumnRenamed("name_new", "name")
   }
 
   /**
-   *  drop 可以删除对应的数据的列的数据的
-   * */
+   * drop 可以删除对应的数据的列的数据的
+   **/
 
   /**
    * 无类型的转换算子实现操作：测试groupBy执行操作语句
-   * */
+   **/
   @Test
-  def testGroupBy(): Unit ={
+  def testGroupBy(): Unit = {
     val persons = Seq(Person("zhangsan", 12), Person("lisi", 18), Person("zhangsan", 8)).toDS()
     //  为什么groupByKey对应的是有类型的操作的。groupBy对应的是无类型的操作的
     // groupByKey 生成的算子是有类型的。可以使用到Person对象的类型的
-   //persons.groupByKey(person=>person.age)
+    //persons.groupByKey(person=>person.age)
     //  根据指定的列执行操作的.groupby对应的算子生成的是无类型的。
     import org.apache.spark.sql.functions._
     // 根据name进行聚合操作，执行sum统计求和操作
@@ -168,29 +170,33 @@ class TypeTransformation {
   /**
    * 在使用无类型的对象的使用，使用的是column对象的
    * 下面是column对象的创建和使用操作的
-   * */
+   **/
   @Test
-  def testCreateColumn(): Unit ={
-      //下面的是column的创建的函数以及操作实现
-      val persons = Seq(Person("zhangsan", 15), Person("lisi", 10)).toDS()
+  def testCreateColumn(): Unit = {
+    //下面的是column的创建的函数以及操作实现
+    val persons = Seq(Person("zhangsan", 15), Person("lisi", 10)).toDS()
     //创建df实现相关的代码的操作实现
-     var df=Seq(("zhangsan", 15),("lisi", 10)).toDF("name","age")
-      // 方式之一:‘创建操作的. symbol对象最终会转化为column对象的
+    var df = Seq(("zhangsan", 15), ("lisi", 10)).toDF("name", "age")
+    // 方式之一:‘创建操作的. symbol对象最终会转化为column对象的
     //  implicit def symbolToColumn(s: Symbol): ColumnName = new ColumnName(s.name)  对应的Symbol对象会转化成为Colume的对象的
-     var column ='name
+    var column = 'name
     //  创建方式之二: $创建对应的操作实现.必须导入spark的隐式转换操作
-    var column1=$"name"
+    var column1 = $"name"
     // 创建方式之三：使用 col方式创建操作,需要导入隐世转换操作的实现
     import org.apache.spark.sql.functions._
-    var   column2=col("name")
+    var column2 = col("name")
     // 创建方式之四:隐式导入functions的函数的
     import org.apache.spark.sql.functions._
-    var   column3=column("name")
+    var column3 = column("name")
     //  ds可以使用column对象的
     persons.select(column).show()
     df.select(column1).show()
-
     // 可以使用其他的算子执行算子操作实现的.实现条件过滤操作的
-    df.where(column==="zhangsan").show()
+    df.where(column === "zhangsan").show()
+    //对应的体现的是column4的操作实现的
+    val column4 = persons.col("name")
+    val column5 = persons.col("name")
+    //确定column对应的是相等的操作实现的
+    println(column4 == column5)
   }
 }

ba1232bf92dc001f8a7dfe5714ded4f4b3115577
mrzhang638993 <1446719386@qq.com>
2020-09-20 15:32:56 +0800

    完成groupby的执行操作实现

---
+++ sparksql/src/main/scala/com/itcast/spark/sparksql/AggrProcessor.scala
@@ -0,0 +1,66 @@
+package com.itcast.spark.sparksql
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{DoubleType, IntegerType, LongType, StructField, StructType}
+import org.junit.Test
+
+/**
+ * spark sql的聚合操作实现
+ * */
+class AggrProcessor {
+
+  val spark: SparkSession =
+    SparkSession.builder().
+      master("local[6]").appName("AggrProcessor")
+    .getOrCreate()
+  // 导入隐式转换
+  import  spark.implicits._
+
+  /**
+   * 数据读取,清洗操作
+   * */
+   @Test
+  def testGroupBy(): Unit ={
+      // groupBy根据指定的列进行分组操作实现
+     // 读取数据执行操作实现
+     val  schema=StructType(
+       //name 和dataType类型的数据执行操作的
+       List(
+         StructField("id",LongType),
+         StructField("year",IntegerType),
+         StructField("month",IntegerType),
+         StructField("day",IntegerType),
+         StructField("hour",IntegerType),
+         StructField("season",IntegerType),
+         // double下面存在NAN的，对应的不是字符串的。
+         StructField("pm",DoubleType)
+       )
+     )
+     val df = spark.read
+       .format("csv")
+       .option("header",value = true)
+       .schema(schema)
+       .csv("D:\\document\\works\\zookeeper-demo\\sparksql\\src\\main\\scala\\com\\itcast\\spark\\sparksql\\beijingpm_with_nan.csv")
+     //  Double.NaN对应的是一个特殊的类型操作的。
+     val filterDf = df.where('pm =!= Double.NaN)
+     //
+     import org.apache.spark.sql.functions._
+     // 按照月份聚合，求解聚合值操作实现
+     //  avg操作返回的是单个的列的数据的。
+     val groupDf = filterDf.groupBy("year", "month")
+     groupDf.agg(avg("pm").as("pm_avg"))
+       .orderBy('pm_avg.desc)
+       .show()
+     //  聚合方式之二:使用groupDataSet的api完成聚合操作实现
+     groupDf.avg("pm")
+       .select($"avg(pm)" as "pm_avg" )
+       .orderBy("pm_avg")
+       .show()
+     //  执行sum操作实现和管理
+     groupDf.sum("pm")
+       .select($"sum(pm)".as("pm_avg"))
+       .orderBy("pm_avg")
+       .show()
+
+   }
+}
---
+++ sparksql/src/main/scala/com/itcast/spark/sparksql/Exec2.scala
@@ -0,0 +1,11 @@
+package com.itcast.spark.sparksql
+
+
+// 我们可能经常需要针对数据进行多维的聚合, 也就是一次性统计小计 ，试写出多维聚合的代码
+// 默写一下课程中使用 SparkSQL 向 MySQL 中写入数据 的代码，写出简要步骤
+// 使用代码读写 Parquet 文件 代码，写出简要框架即可
+// 写出SparkSQL 的 DataFrameReader 读取mysql中spark_test数据库中student表中数据的代码
+class Exec2 {
+
+
+}

82bcdd961a8584c2e2f39da24db87c4cd7b7513a
mrzhang638993 <1446719386@qq.com>
2020-10-06 16:12:52 +0800

    kudu 创建tablet

---
+++ kudu_project/src/main/scala/com.itcast.kudu/KuduApi.scala
@@ -0,0 +1,38 @@
+package com.itcast.kudu
+
+import org.apache.kudu.ColumnSchema.ColumnSchemaBuilder
+import org.apache.kudu.client.KuduClient.KuduClientBuilder
+import org.apache.kudu.client.{CreateTableOptions, KuduClient, KuduTable}
+import org.apache.kudu.{Schema, Type}
+import org.junit.Test
+
+class KuduApi {
+  /**
+   * kudu 创建table
+   * kudu对应的是表的形态的
+   **/
+  @Test
+  def createTable(): Unit = {
+    // 创建表
+    // masterAddresses: String
+    val masterAddress = "192.168.1.205:7051";
+    val kuduClient: KuduClient = new KuduClientBuilder(masterAddress).build()
+    // 创建表的schema
+    // columns: List[ColumnSchema]
+    // ColumnSchema
+    val columnsList = List(
+      new ColumnSchemaBuilder("key", Type.STRING).key(true).build(),
+      new ColumnSchemaBuilder("value", Type.STRING).key(false).build()
+    )
+    import scala.collection.JavaConverters._
+    val schema = new Schema(columnsList.asJava)
+    // 指定分区.指定分区操作操作.默认的分区数是3的。这里只要一个key的
+    val options: CreateTableOptions = new CreateTableOptions().setRangePartitionColumns(List("key").asJava)
+      .setNumReplicas(1)
+    //  创建表
+    // tablet是最终存储数据的，可以存在多个的。之间可以存在主从的概念的。
+    // name: String, schema: Schema, builder: CreateTableOptions
+    val tableName = "simple"
+    val kudeTable: KuduTable = kuduClient.createTable(tableName, schema, options)
+  }
+}

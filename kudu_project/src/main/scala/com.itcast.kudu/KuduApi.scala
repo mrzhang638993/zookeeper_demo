91d3ceae98bf6e00145b04718db3c845321462a5
mrzhang638993 <1446719386@qq.com>
2020-10-06 17:44:56 +0800

    完成kudu的操作代码实现

--- kudu_project/src/main/scala/com.itcast.kudu/KuduApi.scala
+++ kudu_project/src/main/scala/com.itcast.kudu/KuduApi.scala
@@ -2,7 +2,7 @@ package com.itcast.kudu
 
 import org.apache.kudu.ColumnSchema.ColumnSchemaBuilder
 import org.apache.kudu.client.KuduClient.KuduClientBuilder
-import org.apache.kudu.client.{CreateTableOptions, KuduClient, KuduTable}
+import org.apache.kudu.client.{CreateTableOptions, KuduClient}
 import org.apache.kudu.{Schema, Type}
 import org.junit.Test
 
@@ -16,6 +16,7 @@ class KuduApi {
     // 创建表
     // masterAddresses: String
     val masterAddress = "192.168.1.205:7051";
+    // 连接到kube的时候对应的是返回的是一个集群的cluster的。根据master连接到对应的server进行数据处理操作的
     val kuduClient: KuduClient = new KuduClientBuilder(masterAddress).build()
     // 创建表的schema
     // columns: List[ColumnSchema]
@@ -27,12 +28,14 @@ class KuduApi {
     import scala.collection.JavaConverters._
     val schema = new Schema(columnsList.asJava)
     // 指定分区.指定分区操作操作.默认的分区数是3的。这里只要一个key的
-    val options: CreateTableOptions = new CreateTableOptions().setRangePartitionColumns(List("key").asJava)
+    val options: CreateTableOptions = new CreateTableOptions().
+      setRangePartitionColumns(List("key").asJava)
       .setNumReplicas(1)
     //  创建表
     // tablet是最终存储数据的，可以存在多个的。之间可以存在主从的概念的。
     // name: String, schema: Schema, builder: CreateTableOptions
     val tableName = "simple"
-    val kudeTable: KuduTable = kuduClient.createTable(tableName, schema, options)
+    val id: String = kuduClient.createTable(tableName, schema, options).getTableId
+    println(id)
   }
 }

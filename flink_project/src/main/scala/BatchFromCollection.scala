d1b601801c68b6678001c79948dff77e677e125c
mrzhang638993 <1446719386@qq.com>
2020-10-17 15:37:16 +0800

    完成spark的本地数据的读取操作实现

---
+++ flink_project/src/main/scala/BatchFromCollection.scala
@@ -0,0 +1,18 @@
+import org.apache.flink.api.scala.{DataSet, ExecutionEnvironment, _}
+
+object BatchFromCollection {
+
+  def main(args: Array[String]): Unit = {
+    // 创建批处理运行环境
+    val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
+    // 加载本地数据
+    val localElements: DataSet[String] = env.fromElements("spark", "flink", "hadoop")
+    localElements.print()
+    //下面测试读取元组数据
+    val turpleElements: DataSet[(String, Int)] = env.fromElements(("spark", 1), ("flink", 2))
+    turpleElements.print()
+    // 创建dataset数据集
+    val dataset: DataSet[String] = env.fromCollection(List[String]("flink", "spark", "hadoop"))
+    dataset.print()
+  }
+}

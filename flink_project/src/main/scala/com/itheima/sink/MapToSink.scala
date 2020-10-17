2957c28b9f897c275fa282b463dd2be86cff99a3
mrzhang638993 <1446719386@qq.com>
2020-10-17 22:27:55 +0800

    map数据落地

--- flink_project/src/main/scala/com/itheima/sink/MapToSink.scala
+++ flink_project/src/main/scala/com/itheima/sink/MapToSink.scala
@@ -11,12 +11,11 @@ object MapToSink {
   def main(args: Array[String]): Unit = {
     // 创建批处理运行环境
     val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
-    // 设置并行第
-    env.setParallelism(2)
     //  数据的读取操作实现
     val mapValue: DataSet[(Int, String)] = env.fromCollection(Map(1 -> "spark", 2 -> "flink"))
     //  实现数据的落地操作实现
-    mapValue.writeAsText("./dataset/mapToSink/", FileSystem.WriteMode.OVERWRITE)
+    // 设置并行度
+    mapValue.setParallelism(2).writeAsText("./dataset/mapToSink/", FileSystem.WriteMode.OVERWRITE)
     // 上面的操作并没有真正的输出的。
     env.execute("mapToSink")
   }

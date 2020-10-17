ef84803222570f602e1782daf74f7867d96cfb45
mrzhang638993 <1446719386@qq.com>
2020-10-18 07:44:16 +0800

    本地模式启动操作

---
+++ flink_project/src/main/scala/com/itheima/env/BatchLocalEven.scala
@@ -0,0 +1,17 @@
+package com.itheima.env
+
+import org.apache.flink.api.scala.{ExecutionEnvironment, _}
+
+/**
+ * 对应的实现本地操作环境的特性
+ **/
+object BatchLocalEven {
+
+  def main(args: Array[String]): Unit = {
+    // 创建flink的本地运行环境. 设置并行度进行操作实现
+    val env: ExecutionEnvironment = ExecutionEnvironment.createLocalEnvironment().setParallelism(2)
+    //  读取数据
+    val sourceValue: DataSet[Int] = env.fromCollection(List(1, 2, 3, 4))
+    sourceValue.print()
+  }
+}

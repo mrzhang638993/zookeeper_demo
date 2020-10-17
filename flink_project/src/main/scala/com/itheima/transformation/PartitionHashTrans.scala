9f4ec7c7f36a1d8791d2bb61043e75b447e0ce70
mrzhang638993 <1446719386@qq.com>
2020-10-17 21:58:28 +0800

    实现hash分区操作实现

--- flink_project/src/main/scala/com/itheima/transformation/PartitionHashTrans.scala
+++ flink_project/src/main/scala/com/itheima/transformation/PartitionHashTrans.scala
@@ -17,7 +17,7 @@ object PartitionHashTrans {
     env.setParallelism(2)
     // 进行数据分区操作shi.根据指定的key进行分区操作实现
     //val partitionValue: DataSet[Int] = sourceValue.partitionByHash(_.toString)
-    //  不能使用_来代替的，_代表的是遍历过程中的操作的。不能作为hash分区的字段进行操作管理的。
+    //  不能使用_来代替的，turple对应的是根据元组的字段进行分区操作的。
     val partitionValue: DataSet[(String, Int)] = sourceValue.partitionByHash(0)
     partitionValue.writeAsText("./dataset/partitions")
     partitionValue.print()

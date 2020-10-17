d4ede90c8c13085da7969b345df43f1beb378dce
mrzhang638993 <1446719386@qq.com>
2020-10-17 19:56:11 +0800

    数据元素去重操作实现

---
+++ flink_project/src/main/scala/com/itheima/transformation/UnionTrans.scala
@@ -0,0 +1,24 @@
+package com.itheima.transformation
+
+import org.apache.flink.api.scala.{ExecutionEnvironment, _}
+
+/**
+ * union会将数据进行合并操作，但是合并的时候对应的数据是不会去掉的
+ * hadoop
+ * hadoop
+ * hive
+ * hive
+ * flume
+ * spark
+ **/
+object UnionTrans {
+
+  def main(args: Array[String]): Unit = {
+    // 创建批处理运行环境
+    val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
+    val valueDataset: DataSet[String] = env.fromCollection(List("hadoop", "hive", "flume"))
+    val valueDataset1: DataSet[String] = env.fromCollection(List("hadoop", "hive", "spark"))
+    val unionValue: DataSet[String] = valueDataset.union(valueDataset1)
+    unionValue.print()
+  }
+}

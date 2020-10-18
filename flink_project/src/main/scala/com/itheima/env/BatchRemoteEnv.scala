60abdfdd8bd5a2326ef87db2528b913eebd12ab9
mrzhang638993 <1446719386@qq.com>
2020-10-18 09:13:55 +0800

    使用flink的本地模式启动

--- flink_project/src/main/scala/com/itheima/env/BatchRemoteEnv.scala
+++ flink_project/src/main/scala/com/itheima/env/BatchRemoteEnv.scala
@@ -15,7 +15,7 @@ object BatchRemoteEnv {
     val env: ExecutionEnvironment = ExecutionEnvironment.createRemoteEnvironment("cdh1", 8081, "F:\\works\\hadoop1\\zookeeper-demo\\flink_project\\target\\flink_project-1.0-SNAPSHOT.jar")
     // 读取csv文件的内容
     val flinkDatas: DataSet[(Long, String, Long, Float)] = env.readCsvFile[(Long, String, Long, Float)]("hdfs://cdh1:8020/flink_datas/score.csv")
-    val descValue: DataSet[(Long, String, Long, Float)] = flinkDatas.groupBy(_._2).sortGroup(3, Order.DESCENDING).first(1)
+    val descValue: DataSet[(Long, String, Long, Float)] = flinkDatas.groupBy(1).sortGroup(3, Order.DESCENDING).first(1)
     // 根据原子的姓名进行分组操作，根据成绩降序排列操作实现.获取最好的成绩
     descValue.print()
   }

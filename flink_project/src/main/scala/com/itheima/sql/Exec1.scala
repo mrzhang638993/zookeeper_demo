44b055a4601509bad28dfddea1bbbdb3bab520cf
mrzhang638993 <1446719386@qq.com>
2020-10-29 07:07:37 +0800

    完成任务1

--- flink_project/src/main/scala/com/itheima/sql/Exec1.scala
+++ flink_project/src/main/scala/com/itheima/sql/Exec1.scala
@@ -1,5 +1,11 @@
 package com.itheima.sql
 
+import org.apache.flink.core.fs.FileSystem.WriteMode
+import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
+import org.apache.flink.table.api.{Table, TableEnvironment}
+import org.apache.flink.table.api.scala.StreamTableEnvironment
+import org.apache.flink.table.sinks.CsvTableSink
+
 //以 流 处理方式，加载下列数据，并注册为表，查询所有数据，写入到CSV文件中。
 //id     product     amount
 //1         beer             3
@@ -24,6 +30,24 @@ package com.itheima.sql
 //(9,"zhaoliu","2018-10-20 22:30",341.0)
 object Exec1 {
   def main(args: Array[String]): Unit = {
-
+    //  获取流环境
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+    //  获取table执行环境
+    val tableEnv: StreamTableEnvironment = TableEnvironment.getTableEnvironment(env)
+    //  对应的执行环境
+    import org.apache.flink.api.scala._
+    val sourceValue: DataStream[(Int, String, Int)] = env.fromCollection(List(
+      (1, "beer", 3),
+      (2, "diaper", 4),
+      (3, "rubber", 3)
+    ))
+    import org.apache.flink.table.api.scala._
+    tableEnv.registerDataStream("source", sourceValue, 'id, 'product, 'amount)
+    val table: Table = tableEnv.sql("select * from  source")
+    val destValue: DataStream[(Boolean, (Int, String, Int))] = tableEnv.toRetractStream[(Int, String, Int)](table)
+    destValue.print()
+    // 输出结果输出到csv文件中
+    table.writeToSink(new CsvTableSink("./dataset/exec1",",",1,WriteMode.OVERWRITE))
+    env.execute()
   }
 }

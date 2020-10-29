4eafe69c7b82d6b81761843527321536bdd30ef5
mrzhang638993 <1446719386@qq.com>
2020-10-29 21:08:03 +0800

    完成任务1

--- flink_project/src/main/scala/com/itheima/sql/Exec3.scala
+++ flink_project/src/main/scala/com/itheima/sql/Exec3.scala
@@ -1,38 +1,39 @@
 package com.itheima.sql
 
-import org.apache.flink.api.java.tuple.Tuple
-import org.apache.flink.streaming.api.scala.{DataStream, KeyedStream, StreamExecutionEnvironment}
+import org.apache.flink.api.scala.{DataSet, ExecutionEnvironment}
+import org.apache.flink.core.fs.FileSystem.WriteMode
 import org.apache.flink.table.api.{Table, TableEnvironment}
-import org.apache.flink.table.api.scala.StreamTableEnvironment
+import org.apache.flink.table.api.scala.{BatchTableEnvironment}
 
 /**
  * 完成大数据作业3
+ *
+ * 本次答案的适用场景是批处理操作的。不要使用流处理操作的。
  * */
 object Exec3 {
   def main(args: Array[String]): Unit = {
     //  获取流环境
-    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+    val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
     env.setParallelism(1)
     //  获取table执行环境
-    val tableEnv: StreamTableEnvironment = TableEnvironment.getTableEnvironment(env)
+    val tableEnv: BatchTableEnvironment = TableEnvironment.getTableEnvironment(env)
     //  读取数据
-    val sourceValue: DataStream[String] = env.readTextFile("E:\\idea_works\\java\\zookeeper_demo\\flink_project\\src\\main\\scala\\com\\itheima\\logs")
+    val sourceValue: DataSet[String] = env.readTextFile("E:\\idea_works\\java\\zookeeper_demo\\flink_project\\src\\main\\scala\\com\\itheima\\logs")
     //  转换成为对应的case class
     import org.apache.flink.api.scala._
-    val myNetwork: DataStream[(String,String,Int)] = sourceValue.map {
+    // 流式数据不断的产生和增加中的，如何进行统计操作实现的？如何维护一个共同的数据的。需要进行累计求和操作实现的
+    val myNetwork: DataSet[Network] = sourceValue.map {
       it => {
         val arr: Array[String] = it.split(",")
-        (arr(0), arr(1), arr(2).toInt)
+        Network(arr(0), arr(1), arr(2).toInt)
       }
-    }.keyBy(1).reduce((priv, next) => (priv._1, priv._2, priv._3 + next._3))
-    // 注册为表，进行数据统计操作实现
+    }.groupBy("name").reduce((priv,next)=>Network(priv.name,priv.gender,priv.RetainTime+next.RetainTime))
     import org.apache.flink.table.api.scala._
-    tableEnv.registerDataStream("network",myNetwork,'name,'gender,'RetainTime)
-    // 执行sql查询操作
-    val table: Table = tableEnv.sqlQuery("select  name,gender, RetainTime from network  where RetainTime>=120 ")
-    val countValue: DataStream[(Boolean, (String,String,Int))] = table.toRetractStream[(String,String,Int)]
-    val destValue: DataStream[(String,String,Int)] = countValue.map(it => it._2)
-    destValue.print()
+    val tableSink: Table = tableEnv.fromDataSet(myNetwork)
+    val finaltable: Table = tableSink.select('name, 'gender, 'RetainTime)
+      .where('RetainTime >= 120)
+    val value: DataSet[Network] = tableEnv.toDataSet[Network](finaltable)
+    value.writeAsText("./dataset/exec2",WriteMode.OVERWRITE)
     env.execute()
   }
 }

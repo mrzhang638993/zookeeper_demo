e588d8d8f35b3ed23e2d5cc6e660d254678a5070
mrzhang638993 <1446719386@qq.com>
2021-09-05 19:22:59 +0800

    set相关集合的操作和实现

--- scala_basis/src/main/scala/com.itheima/SetOperation.scala
+++ scala_basis/src/main/scala/com.itheima/SetOperation.scala
@@ -1,5 +1,7 @@
 package com.itheima
 
+import scala.collection.mutable
+
 /**
  * set 定义了不可变的集合元素
  * 元素是不保证数据的，数据不会重复的
@@ -10,7 +12,7 @@ object SetOperation {
     /*val ints = Set[Int](1, 1)// 实际底层的元素还是只有一个元素的。
     ints.foreach(println(_))*/
     // set的相关的操作
-    val ints = Set(1, 1, 2, 3, 4, 5)
+   /* val ints = Set(1, 1, 2, 3, 4, 5)
     println(ints.size)
     println("$$$$$$$$$$$$$$$$$")
     for(ele <- ints)println(ele)
@@ -21,6 +23,15 @@ object SetOperation {
     val ints2 = List(6, 7, 8, 9)
     val setList=ints++ints2
     println("$$$$$$$$$$$$$$$$$")
-    setList.foreach(println(_))
+    setList.foreach(println(_))*/
+    // 集合操作之间的++操作适用于所有的集合之间元素之间的操作实现的。
+    //可变集合的操作管理,对于可变集合的操作处理实现
+    val ints: mutable.Set[Int] = scala.collection.mutable.Set(1, 2, 3, 4)
+    ints+=5 //增加元素实现相关的元素的操作实现
+    println("$$$$$$$$$$$$$$$$")
+    ints.foreach(println(_))
+    ints -=1 // 移除元素
+    println("$$$$$$$$$$$$$$$$")
+    ints.foreach(println(_))
   }
 }

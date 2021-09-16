14653022592f487672a4d61f1675d25fdc3ac6ea
mrzhang638993 <1446719386@qq.com>
2021-09-16 21:23:34 +0800

    调用单例对象的成员方法和相关的代码操作实践和处理操作的

---
+++ scala_basis/src/main/scala/com.itheima/PrintUtilsDemo.scala
@@ -0,0 +1,12 @@
+package com.itheima
+
+/**
+ * 调用单例对象的成员变量和方法实现相关的操作实现的
+ * */
+object PrintUtilsDemo {
+
+  def main(args: Array[String]): Unit = {
+    //调用单例对象的成员方法完成对应的打印操作实现的
+     PrintUtils.printLog()
+  }
+}

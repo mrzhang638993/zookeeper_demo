affefd7d005f7ed8a0ec87b413e0430c13ca9e0e
mrzhang638993 <1446719386@qq.com>
2021-09-17 21:05:10 +0800

    对象的main方法以及相关的App的trait的特质继承信息的

---
+++ scala_basis/src/main/scala/com.itheima/MainDemo.scala
@@ -0,0 +1,10 @@
+package com.itheima
+
+/**
+ * 实现相关的代码继承操作实现逻辑
+ * App底层是实现了相关的main方法的。
+ * 里面的代码和书写在main方法中的代码是一样的。
+ * */
+object MainDemo  extends App {
+  println("hello")
+}
---
+++ scala_basis/src/main/scala/com.itheima/TestMainObject.scala
@@ -0,0 +1,12 @@
+package com.itheima
+
+/**
+ * 测试object的main方法和相关的操作
+ * */
+object TestMainObject {
+  // 测试相关的main方法和实践操作实现的。
+  //  scala需要实现类似的静态的方法的操作实现的,需要使用object对象来实现相关的操作来实现的
+  def main(args: Array[String]): Unit = {
+      println("hello scala")
+  }
+}

a52f7ba3f3b199586910a97bdebc9578152c2a4e
mrzhang638993 <1446719386@qq.com>
2021-09-19 12:54:01 +0800

    对象的main方法以及相关的App的trait的特质继承信息的

---
+++ scala_basis/src/main/scala/extend/Person.scala
@@ -0,0 +1,26 @@
+package extend
+
+class Person{
+    var name:String=_
+
+   def  getName():String={
+      name
+   }
+}
+
+class Student extends  Person {
+    //子类无法查看到父类的name属性信息。
+    //scala中的属性操作不需要set方法的，只需要对象名称.属性名称即可了。
+    def setName(name:String): Unit ={
+       this.name=name
+    }
+}
+//相关的测试代码和实现体系以及机制操作
+//完成相关的代码实现和机制操作。
+object TestExtend{
+  def main(args: Array[String]): Unit = {
+    val student = new Student
+    student.name="lisi"
+    println(student.name)
+  }
+}

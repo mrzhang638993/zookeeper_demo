65e7d0e20b11e941c4d2b29f99ef8ac374b6587b
mrzhang638993 <1446719386@qq.com>
2021-09-21 09:18:29 +0800

    完成相关的特质的实现代码

---
+++ scala_basis/src/main/scala/traits/Loggeres.scala
@@ -0,0 +1,18 @@
+package traits
+//定义相关的特质操作实现
+trait Loggeres {
+   def log(msg:String)
+}
+class UserServerviceLogger
+object TestHunru{
+  def main(args: Array[String]): Unit = {
+    //定义对象的时候混入相关的特质的操作逻辑
+    val logger = new UserServerviceLogger with Loggeres {
+      override def log(msg: String): Unit = {
+         println(s"测试相关的混入操作逻辑和实现:${msg}")
+      }
+    }
+    //执行数据的打印操作实现逻辑和相关的体现
+    logger.log("hello world")
+  }
+}

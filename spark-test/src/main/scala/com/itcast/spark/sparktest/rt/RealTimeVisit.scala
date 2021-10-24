0f2b0dfe11ba18724dfc2ac1c9eaf7bd6ee46e2f
mrzhang638993 <1446719386@qq.com>
2021-10-24 09:59:29 +0800

    聚合进行计算操作实现

--- spark-test/src/main/scala/com/itcast/spark/sparktest/rt/RealTimeVisit.scala
+++ spark-test/src/main/scala/com/itcast/spark/sparktest/rt/RealTimeVisit.scala
@@ -1,11 +1,17 @@
 package com.itcast.spark.sparktest.rt
 
-import org.apache.kafka.clients.consumer.ConsumerConfig
+import com.itcast.spark.sparktest.analysis.DateUtils
+import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord}
 import org.apache.kafka.common.TopicPartition
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.streaming.kafka010.{KafkaUtils, LocationStrategy}
-import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.dstream.{DStream, InputDStream, MapWithStateDStream}
+import org.apache.spark.streaming.kafka010.{ConsumerStrategies, ConsumerStrategy, KafkaUtils, LocationStrategies}
+import org.apache.spark.streaming.{Seconds, State, StateSpec, StreamingContext}
+import org.apache.spark.util.LongAccumulator
+
+import java.util.Date
+import scala.collection.immutable
 
 /**
  * 实时访问量数据统计操作实现
@@ -47,6 +53,45 @@ object RealTimeVisit {
     //需要指定spark  streaming
     //需要指定保存的一致性的配置
     //指定kafka的参数配置
-    KafkaUtils.createDirectStream[String,String](context,LocationStrategy)
+    val partitions: immutable.Iterable[TopicPartition] = partitionToLong.map(part => part._1)
+    val consumerStrategy: ConsumerStrategy[String, String] = ConsumerStrategies.Assign[String, String](partitions, kafkaParams, partitionToLong)
+    //获取kafka中的流式数据信息
+    val message: InputDStream[ConsumerRecord[String, String]] = KafkaUtils.createDirectStream[String, String](context, LocationStrategies.PreferConsistent, consumerStrategy)
+    //处理数据信息,根据课程id,sessionId,ip去重,实现实时操作
+    //根据课程id,sessionId,ip实现计数功能操作实现
+    val userVisit: DStream[(String, Int)] = message.map(_.value()).map(_.split("\t")).map(arr => {
+      //获取得到groupKey的操作的数据结果信息,对应的三个作为一组的话认为是一个相同的浏览人数的信息
+      val groupkey: String = arr(0) + "_" + arr(2) + "+" + arr(3)
+      (groupkey, 1)
+    })
+    //定义去重逻辑和操作实现,其中State[Int]对应的是处理分布式情况下面的数据处理逻辑操作
+    //流式环境下面的操作需要的是state实现数据的协调操作实现的。
+    val mapWithStateMethod=(word:String,count:Option[Int],state:State[Int])=>{
+        //计数之间进行数据的更新操作和实现
+        val sum=count.getOrElse(0)+state.getOption().getOrElse(0)
+        //更新计数的数据信息
+        state.update(sum)
+        //返回更新之后的数据的,返回更新之后的数据操作结果来实现数据的更新操作和实现的
+        (word,sum)
+    }
+    //对应的还是一个计数操作的
+    val destUserVisit: MapWithStateDStream[String, Int, Int, (String, Int)] = userVisit.mapWithState(StateSpec.function(mapWithStateMethod))
+    //共享变量,获取spark的共享变量实现操作
+    val uv: LongAccumulator = spark.sparkContext.longAccumulator("uv")
+    val keyInfo="visit_date_"+DateUtils.getDateStr(new Date().getTime,"yyyy-MM-dd")
+    //对应的完成相关的数据统计操作和实现逻辑
+    val str: String = redisUtil.getResultRedis(rt_cv_key, keyInfo)
+    if(str!=null){
+      uv.add(str.toLong)
+    }
+    //聚合进行计数操作
+    //对于uv的数据进行聚合操作实现
+    destUserVisit.foreachRDD(iter=>{
+       //数量大于0的话执行聚合操作。
+       if(iter.count()>0){
+         uv.add(iter.count())
+         redisUtil.storeOffsetRedis(rt_cv_key,keyInfo,uv.value)
+       }
+    })
   }
 }

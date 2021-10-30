999f08e9a362ca96e95fc8e0996c57766a9a0f6f
mrzhang638993 <1446719386@qq.com>
2021-10-30 19:07:32 +0800

    hbase的rdd数据保存机制

--- spark-test/src/main/scala/com/itcast/spark/sparktest/search/InstantQueryStep2ToHbaseAndES.scala
+++ spark-test/src/main/scala/com/itcast/spark/sparktest/search/InstantQueryStep2ToHbaseAndES.scala
@@ -11,6 +11,7 @@ import org.elasticsearch.spark.sql.EsSparkSQL
  *数据保存到hbase以及es中进行数据保存操作实现
  *hbase中保存的是完整的数据的,后续的话可以实现根据数据来进行数据的索引操作实现的
  *es中存在的是rowkey的,hbase中存在的是完整的数据备份的。
+ * 第一步创建和建立中间的表数据信息,后续操作从中间表读物数据来写入到es以及hbase中完成相关的操作的。
  * */
 object InstantQueryStep2ToHbaseAndES {
   def main(args: Array[String]): Unit = {
